43517971f20e836d78fb555ce89d4f41b61f5035
RamonPontes <rgdpontes@yahoo.com>
2024-10-03 14:17:43 -0300

    Integrado back-end com o front-end

--- client/src/app/user/traking/[code]/page.js
+++ client/src/app/user/traking/[code]/page.js
@@ -1,6 +1,19 @@
+'use client'
+
+import { useEffect } from 'react'
 import './style.css'
+import api from "@/app/services/api"
 
 export default function Traking({ params }) {
+    useEffect(() => {
+        api
+            .get("/")
+            .then((response) => console.log(response.data))
+            .catch((err) => {
+            console.error(err)
+            })
+    }, [])
+
     return(
         <div className="traking-container">
             <div className='traking-itens'>
@@ -9,102 +22,6 @@ export default function Traking({ params }) {
                     <p>Ponto de origem</p>
                     <p>Status</p>
                     <p>Ponto Destino</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
-                </div>
-                <div className="item">
-                    <p>01/01/24 14:50</p>
-                    <p>Ponta Grossa</p>
-                    <p>TRANSFERENCIA</p>
-                    <p>Curitiba</p>
                 </div>
             </div>
         </div>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
 
 app.get('/', function (req, res) {
     res.send('Hello World')
