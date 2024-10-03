862a2f78b50935aa8af055536723b93badf869e2
RamonPontes <rgdpontes@yahoo.com>
2024-10-03 15:08:39 -0300

    .

--- client/src/app/user/traking/[code]/page.js
+++ client/src/app/user/traking/[code]/page.js
@@ -1,29 +1,39 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './style.css'
 import api from "@/app/services/api"
 
 export default function Traking({ params }) {
+
+    const [error, setError] = useState({})
+
     useEffect(() => {
         api
-            .get("/traking", { trakingCode: '01' })
+            .get(`/traking?trakingCode=${params.code}`, { trakingCode: '01' })
             .then((response) => console.log(response.data))
             .catch((err) => {
-            console.error(err)
+                setError({
+                    status: 500,
+                    message: 'Erro interno entre em contato com o suporte!'
+                })
             })
     }, [])
 
-    return(
+    return (
         <div className="traking-container">
-            <div className='traking-itens'>
-                <div className="top">
-                    <p>Data/Hora</p>
-                    <p>Ponto de origem</p>
-                    <p>Status</p>
-                    <p>Ponto Destino</p>
+            {Object.keys(error).length > 0 && <h1>{error.status + ' - ' + error.message}</h1>}
+
+            {Object.keys(error).length == 0 &&
+                <div className='traking-itens'>
+                    <div className="top">
+                        <p>Data/Hora</p>
+                        <p>Ponto de origem</p>
+                        <p>Status</p>
+                        <p>Ponto Destino</p>
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     )
 }
